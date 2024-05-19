0753b57676463a2fcb7134fdf75c332a059be3c7
Kenny Clayton <kenny.d.clayton@gmail.com>
2024-05-19 19:29:28 -0400

    first commit

---
+++ Study Tracker/index.js
@@ -0,0 +1,11 @@
+function increment() {
+    console.log("The button was clicked");
+  }
+
+let popAlertMessage1 = document.getElementById("pop-up-btn");
+console.log(popAlertMessage1)
+
+function alert1() {
+  window.alert(popAlertMessage1.textContent = "You created a pop-up! Nice Work")
+}
+
